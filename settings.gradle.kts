8cb784b776dc0296f781b0436369324c0cee3131
khebrati <khebrati@gmail.com>
2024-01-03 17:52:15 +0330

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "KMP-library"
+includeBuild("convention-plugins")
+include(":shared")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
